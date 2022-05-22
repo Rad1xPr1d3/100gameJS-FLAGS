d4b42808dc64c7a34d49417e1e85cabb426dac59
Andrey <andrey.koreshkoff@gmail.com>
2022-05-22 14:06:51 +0300

    feat: add random img and buttons 20220522 Sun 14:06

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 // connect JSON
-let DB;
 (async () => {
-    DB = await(await fetch('flags.json')).json(); //? разобраться как это работает
-     console.log(DB.Aruba.Flag);
-     console.log(DB.Aruba.Flag);
+    let DB = await(await fetch('flags.json')).json(); //? разобраться как это работает
     let flag = document.getElementById('flag');
-    let newFlag = DB.Aruba.Flag;
+    let buttons = document.getElementsByClassName("button");
+    let randomNumber = randIntExcep(1, 3);
+    let newFlag = DB[randomNumber].Flag;
     flag.setAttribute('src', newFlag);
+
+        for (let item of buttons) {
+        item.innerHTML = DB[randomNumber].Country;
+        randomNumber = randIntExcep(1, 3);
+    }
+
+
+    function randIntExcep(min, max, exp) {
+        var n,
+            exp = Array.isArray(exp) ? exp : [(isNaN(exp) ? min-1 : exp)];
+        while(true){
+            n = Math.floor(Math.random() * (max - min + 1)) + min;
+            if(exp.indexOf(n) < 0) return n;
+        }
+    }
 })();
-
-
