aaa019d3bf103897fb6748431dce7a75607762f3
Andrey <andrey.koreshkoff@gmail.com>
2022-05-22 15:30:20 +0300

    refactor: add 2cards to JSON 20220522 Sun 15:30

--- script.js
+++ script.js
@@ -3,16 +3,22 @@
     let DB = await(await fetch('flags.json')).json(); //? разобраться как это работает
     let flag = document.getElementById('flag');
     let buttons = document.getElementsByClassName("button");
-    let randomNumber = randIntExcep(1, 3);
+    let randomNumber = randIntExcep(1, 5);
     let newFlag = DB[randomNumber].Flag;
     flag.setAttribute('src', newFlag);
 
-        for (let item of buttons) {
+    for (let item of buttons) {
         item.innerHTML = DB[randomNumber].Country;
-        randomNumber = randIntExcep(1, 3);
+        randomNumber = randIntExcep(1, 5);
     }
+    let randomButtons = randIntExcep(0, 2);
+    buttons[randomButtons] = DB[randomNumber].Country;
 
 
+    console.log(randomButtons);
+    console.log(randomNumber);
+
+// RANDOM!
     function randIntExcep(min, max, exp) {
         var n,
             exp = Array.isArray(exp) ? exp : [(isNaN(exp) ? min-1 : exp)];
@@ -21,4 +27,5 @@
             if(exp.indexOf(n) < 0) return n;
         }
     }
+// END RANDOM!
 })();
